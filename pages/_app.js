1e76d3a9b184421758d851a5ec37a772fb239b0b
Daryl Pinto <daryl.j.pinto@gmail.com>
2019-05-29 10:51:23 -0400

    Add husky & lint-staged

--- pages/_app.js
+++ pages/_app.js
@@ -1,117 +1,116 @@
-import React from "react"; // eslint-disable-line no-unused-vars
 import App, { Container } from "next/app";
 import "globals/styles/_reset.css";
 import "globals/styles/style.css";
 
 class MainApp extends App {
-  static async getInitialProps({ Component, ctx }) {
-    let pageProps = {};
+	static async getInitialProps({ Component, ctx }) {
+		let pageProps = {};
 
-    if (Component.getInitialProps) {
-      pageProps = await Component.getInitialProps(ctx);
-    }
+		if (Component.getInitialProps) {
+			pageProps = await Component.getInitialProps(ctx);
+		}
 
-    return { pageProps };
-  }
+		return { pageProps };
+	}
 
-  render() {
-    const { Component, pageProps } = this.props;
+	render() {
+		const { Component, pageProps } = this.props;
 
-    return (
-      <Container>
-        <main>
-          {/* Header */}
-          <header>
-            <img src="https://placehold.it/100x50" />
-            <div className="menu-button">
-              <span />
-            </div>
-          </header>
+		return (
+			<Container>
+				<main>
+					{/* Header */}
+					<header>
+						<img src="https://placehold.it/100x50" />
+						<div className="menu-button">
+							<span />
+						</div>
+					</header>
 
-          {/* Pages render here */}
-          <Component {...pageProps} />
+					{/* Pages render here */}
+					<Component {...pageProps} />
 
-          {/* Footer */}
-          <footer>
-            <div className="copyright">
-              © {new Date().getFullYear()} All rights reserved
-            </div>
-            <div className="social">
-              <a
-                href="https://www.instagram.com/snakebyte.studios/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <i className="fab fa-instagram" />
-              </a>
-            </div>
-          </footer>
+					{/* Footer */}
+					<footer>
+						<div className="copyright">
+							© {new Date().getFullYear()} All rights reserved
+						</div>
+						<div className="social">
+							<a
+								href="https://www.instagram.com/snakebyte.studios/"
+								target="_blank"
+								rel="noopener noreferrer"
+							>
+								<i className="fab fa-instagram" />
+							</a>
+						</div>
+					</footer>
 
-          {/* Styles for main app layout */}
-          <style jsx>{`
-            header {
-              display: flex;
-              justify-content: space-between;
-              align-items: center;
-              padding: 20px;
-              position: fixed;
-              top: 0;
-              left: 0;
-              height: 90px;
-              width: 100vw;
-            }
+					{/* Styles for main app layout */}
+					<style jsx>{`
+						header {
+							display: flex;
+							justify-content: space-between;
+							align-items: center;
+							padding: 20px;
+							position: fixed;
+							top: 0;
+							left: 0;
+							height: 90px;
+							width: 100vw;
+						}
 
-            .menu-button {
-              padding: 10px 0;
-              cursor: pointer;
-            }
+						.menu-button {
+							padding: 10px 0;
+							cursor: pointer;
+						}
 
-            .menu-button span,
-            .menu-button span:before,
-            .menu-button span:after {
-              display: block;
-              height: 5px;
-              width: 30px;
-              position: relative;
-              background-color: black;
-            }
+						.menu-button span,
+						.menu-button span:before,
+						.menu-button span:after {
+							display: block;
+							height: 5px;
+							width: 30px;
+							position: relative;
+							background-color: black;
+						}
 
-            .menu-button span:before {
-              content: "";
-              position: absolute;
-              top: -10px;
-            }
+						.menu-button span:before {
+							content: "";
+							position: absolute;
+							top: -10px;
+						}
 
-            .menu-button span:after {
-              content: "";
-              position: absolute;
-              top: 10px;
-            }
+						.menu-button span:after {
+							content: "";
+							position: absolute;
+							top: 10px;
+						}
 
-            footer {
-              display: flex;
-              justify-content: space-between;
-              align-items: center;
-              position: fixed;
-              bottom: 0;
-              left: 0;
-              width: 100vw;
-              padding: 15px;
-            }
+						footer {
+							display: flex;
+							justify-content: space-between;
+							align-items: center;
+							position: fixed;
+							bottom: 0;
+							left: 0;
+							width: 100vw;
+							padding: 15px;
+						}
 
-            footer .social a {
-              padding: 5px;
-              color: black;
-            }
+						footer .social a {
+							padding: 5px;
+							color: black;
+						}
 
-            footer .social i {
-              font-size: 24px;
-            }
-          `}</style>
-        </main>
-      </Container>
-    );
-  }
+						footer .social i {
+							font-size: 24px;
+						}
+					`}</style>
+				</main>
+			</Container>
+		);
+	}
 }
 
 export default MainApp;
--- pages/index.js
+++ pages/index.js
@@ -3,80 +3,80 @@ import Head from "next/head";
 import Link from "next/link";
 
 class Home extends Component {
-  render() {
-    return (
-      <React.Fragment>
-        {/* Meta content */}
-        <Head>
-          <title>Snakebyte Studios</title>
-        </Head>
+	render() {
+		return (
+			<React.Fragment>
+				{/* Meta content */}
+				<Head>
+					<title>Snakebyte Studios</title>
+				</Head>
 
-        {/* Page content */}
-        <main>
-          <div id="logo">
-            <div className="brandmark" />
-            <h1>Snakebyte Studios</h1>
-          </div>
+				{/* Page content */}
+				<main>
+					<div id="logo">
+						<div className="brandmark" />
+						<h1>Snakebyte Studios</h1>
+					</div>
 
-          <nav>
-            <Link href="/team">
-              <a>Team</a>
-            </Link>
-            <Link href="/portfolio">
-              <a>Portfolio</a>
-            </Link>
-            <Link href="/contact">
-              <a>Contact</a>
-            </Link>
-          </nav>
-        </main>
+					<nav>
+						<Link href="/team">
+							<a>Team</a>
+						</Link>
+						<Link href="/portfolio">
+							<a>Portfolio</a>
+						</Link>
+						<Link href="/contact">
+							<a>Contact</a>
+						</Link>
+					</nav>
+				</main>
 
-        {/* CSS */}
-        <style jsx>{`
-          main {
-            display: flex;
-            flex-direction: column;
-            height: 100vh;
-            justify-content: center;
-            align-items: center;
-          }
+				{/* CSS */}
+				<style jsx>{`
+					main {
+						display: flex;
+						flex-direction: column;
+						height: 100vh;
+						justify-content: center;
+						align-items: center;
+					}
 
-          #logo {
-            display: flex;
-            align-items: center;
-          }
+					#logo {
+						display: flex;
+						align-items: center;
+					}
 
-          #logo .brandmark {
-            height: 60px;
-            width: 70px;
-            background-color: black;
-          }
+					#logo .brandmark {
+						height: 60px;
+						width: 70px;
+						background-color: black;
+					}
 
-          #logo h1 {
-            font-size: 60px;
-            margin-left: 10px;
-          }
+					#logo h1 {
+						font-size: 60px;
+						margin-left: 10px;
+					}
 
-          nav {
-            margin-top: 40px;
-          }
+					nav {
+						margin-top: 40px;
+					}
 
-          nav a {
-            text-decoration: none;
-            color: black;
-            text-transform: uppercase;
-            margin-right: 60px;
-            font-weight: bold;
-            font-size: 20px;
-          }
+					nav a {
+						text-decoration: none;
+						color: black;
+						text-transform: uppercase;
+						margin-right: 60px;
+						font-weight: bold;
+						font-size: 20px;
+					}
 
-          nav a:last-of-type {
-            margin-right: 0;
-          }
-        `}</style>
-      </React.Fragment>
-    );
-  }
+					nav a:last-of-type {
+						margin-right: 0;
+					}
+				`}</style>
+			</React.Fragment>
+		);
+	}
 }
 
 export default Home;
