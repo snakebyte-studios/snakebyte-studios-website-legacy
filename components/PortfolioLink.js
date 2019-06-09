2f1a6d2290b5ec12ef7cb74045abfa3a91106653
Daryl Pinto <daryl.j.pinto@gmail.com>
2019-06-09 01:41:39 -0400

    Fix portfolio link spacing, minor code cleanup

--- components/PortfolioLink.js
+++ components/PortfolioLink.js
@@ -1,34 +1,19 @@
-const PortfolioLink = () => {
+const PortfolioLink = ({ name, image, description, link }) => {
 	return (
-		<React.Fragment>
-			<main>
-				<div className="container">
-					<div className="card">
-						<img
-							className="cardImage"
-							src="https://cdn.shopify.com/s/files/1/0533/2089/files/web-design-portfolio-inspiration-toyfight.png?v=1503510344"
-						/>
-						<div className="cardBody">
-							<h5 className="cardTitle">Sample Website</h5>
-							<p className="cardDesc">
-								This is a sample site made for a cool company
-							</p>
-							<a href="#" className="visitButton">
-								Visit
-							</a>
-						</div>
-					</div>
+		<>
+			<div className="card">
+				<img src={image} alt={name} />
+				<div className="info">
+					<h5>{name}</h5>
+					<p>{description}</p>
+					<a href={link} target="_blank" rel="noopener noreferrer">
+						Visit
+					</a>
 				</div>
-			</main>
+			</div>
 
 			<style jsx>{`
-				.container {
-					display: flex;
-					align-items: center;
-				}
-
 				.card {
-					width: 30%;
 					background-color: #f4f7f6;
 					align-content: center;
 					justify-content: center;
@@ -36,9 +21,7 @@ const PortfolioLink = () => {
 					padding: 20px;
 				}
 
-				.cardImage {
-					/*   position: relative; */
-					height: 30%;
+				.card img {
 					width: 100%;
 					display: block;
 					object-fit: cover;
@@ -46,52 +29,45 @@ const PortfolioLink = () => {
 					transition: transform 0.7s, filter 0.7s ease-in-out;
 					filter: blur(0);
 					transform: scale(1);
-
 					margin-bottom: 15px;
 				}
 
-				.cardTitle {
-					margin-bottom: 10px;
-				}
-
-				.cardDesc {
-					margin-bottom: 10px;
-				}
-
-				/* .cardImage img {
-     transition: transform 0.7s, filter 0.7s ease-in-out;
-     filter: blur(0);
-     transform: scale(1);
-
-} */
-
-				.cardImage:hover {
+				.card img:hover {
 					filter: blur(2px);
 					transform: scale(1.2);
 				}
 
-				.cardBody {
-					/*   padding-left: 20px; */
-					display: flex;
-					flex-direction: column;
-					align-items: flex-start;
+				.card h5 {
+					font-size: 18px;
+					margin-bottom: 10px;
 				}
 
-				.visitButton {
-					width: 50%;
-					/*   left: 50%; */
-					position: relative;
-					text-align: center;
+				.card p {
+					font-size: 16px;
+					line-height: 1.4em;
+					margin-bottom: 10px;
+				}
+
+				/*
+				.cardImage img {
+					transition: transform 0.7s, filter 0.7s ease-in-out;
+					filter: blur(0);
+					transform: scale(1);
+				}
+				*/
+
+				.card a {
+					display: inline-block;
 					text-decoration: none;
 					/*   border: 1px solid black; */
-					/*   padding: 5px 10px 5px 10px; */
+					padding: 10px 30px;
 					border-radius: 20px;
 					background: orange;
 					color: white;
 					font-size: 1em;
 				}
 			`}</style>
-		</React.Fragment>
+		</>
 	);
 };
 
--- pages/portfolio.js
+++ pages/portfolio.js
@@ -3,10 +3,10 @@ import PortfolioLink from "components/PortfolioLink.js";
 
 const Portfolio = () => {
 	return (
-		<React.Fragment>
+		<>
 			{/* Meta content */}
 			<Head>
-				<title>Our Blog - Snakebyte Studios</title>
+				<title>Portfolio - Snakebyte Studios</title>
 				<meta
 					name="description"
 					content="Years of experience and keen eyes for detail –
@@ -14,23 +14,49 @@ const Portfolio = () => {
 				/>
 			</Head>
 
-			<main>
+			<main id="portfolio-page">
 				<div className="container">
-					<PortfolioLink />
-					<PortfolioLink />
+					<PortfolioLink
+						name="Sample Website 1"
+						image="https://cdn.shopify.com/s/files/1/0533/2089/files/web-design-portfolio-inspiration-toyfight.png?v=1503510344"
+						description="This is a sample site made for a cool company"
+						link="#"
+					/>
+					<PortfolioLink
+						name="Sample Website 2"
+						image="https://cdn.shopify.com/s/files/1/0533/2089/files/web-design-portfolio-inspiration-toyfight.png?v=1503510344"
+						description="This is a sample site made for a totally different company"
+						link="#"
+					/>
 				</div>
 			</main>
 
 			<style jsx>{`
+				#portfolio-page {
+					min-height: 100vh;
+				}
+
 				.container {
 					display: flex;
 					align-items: center;
-					justify-content: space-between;
+					flex-wrap: wrap;
+					width: var(--container-width);
+					max-width: 80%;
+					margin: 0 auto;
+					margin-top: 100px;
+				}
 
-					height: 100vh;
+				.container :global(.card) {
+					width: calc(33% - 20px);
+					margin-right: 20px;
+					margin-bottom: 20px;
+				}
+
+				.container :global(.card:nth-of-type(3n)) {
+					margin-right: 0px;
 				}
 			`}</style>
-		</React.Fragment>
+		</>
 	);
 };
 
