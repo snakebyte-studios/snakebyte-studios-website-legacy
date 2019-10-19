5d42b8d3f6eff00a3d0e04e86db42305837ec7b6
Kumail <knanji123@gmail.com>
2019-10-19 17:55:05 -0400

    Code Cleanup

--- components/DesignLink.js
+++ components/DesignLink.js
@@ -4,21 +4,10 @@ const DesignLink = ({ name, image, description, link }) => {
 			<div className="card">
 				<a href={link}>
 					<img src={image} alt={name} />{" "}
-					{/* <video loop autoPlay muted width="320" visible={notAvideo}>
-						<source src={video} type="video/mp4" alt={name} />
-					</video> */}
 				</a>
 				<div className="info">
 					<h5>{name}</h5>
 					<p>{description}</p>
-					{/* <div
-						className="link"
-						target="_blank"
-						rel="noopener noreferrer"
-						onClick="setModalOpen(true)"
-					>
-						Visit
-					</div> */}
 				</div>
 			</div>
 
