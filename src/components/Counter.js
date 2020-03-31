9453400ac10ad0ffd252d00c20fbb97b28c959ab
Lev Lionn <levster81@gmail.com>
2020-03-30 21:31:50 -0400

    learned some state shtff

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,12 +1,64 @@
-import React, { useState } from "react";
+import { useState } from "react";
+
+let blogContent = [
+	{
+		id: 0,
+		title: "something",
+		author: "Daryl Pintooo",
+		content: "this is the body paragprah.."
+	},
+	{
+		id: 1,
+		title: "something to do",
+		author: "He-man",
+		content: "he-man is a sea-man"
+	},
+	{
+		id: 2,
+		title: "something to see ",
+		author: "Baby Yoda",
+		content: "study state you must"
+	},
+	{
+		id: 3,
+		title: "something somewhere",
+		author: "NerdBoi",
+		content:
+			"Can tell you the difference between AMD and INTEL's new CPU off the cuff"
+	},
+	{
+		id: 4,
+		title: "something all along",
+		author: "Chad Chaderson",
+		content: "Has slept with step-sister"
+	}
+];
 
 function Counter() {
-	const [count, setCount] = useState(0);
+	const [selectedPostId, setSelectedPostId] = useState(0);
+
+	let activePost = blogContent.find(r => r.id == selectedPostId);
 
 	return (
 		<div className="counter">
-			<p>This is the current count: {count}</p>
+			<ul>
+				{blogContent.map(blogPost => (
+					<li>
+						<h2 onClick={() => setSelectedPostId(blogPost.id)}>
+							{blogPost.title}
+						</h2>
+					</li>
+				))}
+			</ul>
+			<hr style={{ border: "1px solid black" }} />
+			<hr style={{ border: "1px solid black" }} />
+			<hr style={{ border: "1px solid black" }} />
 
+			<section>
+				<h1>{activePost.title}</h1>
+				<p>{activePost.content}</p>
+				<span>{activePost.author}</span>
+			</section>
 			<style jsx>{`
 				.counter {
 					position: fixed;
