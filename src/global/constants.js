e086248760905ff92fe89f0a99b7f8dcffdfc4c3
Daryl Pinto <daryl.j.pinto@gmail.com>
2020-08-29 21:17:56 -0400

    fix: contact form backend

--- src/global/constants.js
+++ src/global/constants.js
@@ -7,5 +7,10 @@ export const KEYS = {
 	DOWNARROW: 40
 };
 
+// "From" email must be verified as a single-sender in sendgrid
+// https://app.sendgrid.com/settings/sender_auth
+export const CONTACT_FORM_SENDER_EMAIL = "snakebyte.studios.dev@gmail.com";
+export const CONTACT_FORM_RECIEVING_EMAIL = "info@snakebyte.ca";
+
 export const RECAPTCHA_SITE_KEY = "6LcmVb4UAAAAAHcQ4YFkIwJIh8l7-CXLZKdIkFek";
 export const ANALYTICS_TRACKING_CODE = "UA-150354503-1";
